2f0d09faa569dd42a9443f2ce8c0d64a9e606bda
ion <ionix@protonmail.com>
2016-12-31 19:48:35 +0100

    Add project files.

---
+++ Terraria Patcher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terraria_Patcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
